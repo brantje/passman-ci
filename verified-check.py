789c198220e66a8d709abc5ab055b90bcfcb3b8b
Sander <brantje@gmail.com>
2017-07-28 16:45:21 +0200

    Update verified-check.py

--- verified-check.py
+++ verified-check.py
@@ -3,48 +3,65 @@ import json
 import sys
 import os
 
+from pprint import pprint
+
+
 pullRequestNumber = os.environ.get('DRONE_PULL_REQUEST')
 repoOwner = os.environ.get('DRONE_REPO_OWNER')
 repoName = os.environ.get('DRONE_REPO_NAME')
 oauthToken = os.environ.get('OAUTH_TOKEN','')
 droneEvent = os.environ.get('DRONE_BUILD_EVENT')
 
-isPrValid = False
+
 def getData(url):
     headers = {'content-type': 'application/json', 'Accept': 'application/vnd.github.cryptographer-preview'}
     if oauthToken:
-        print("Found oauth token!")
         headers['Authorization'] = 'token '+ oauthToken
     response = requests.get(url, headers=headers)
     return json.loads(response.text)
 
+def checkCommit(commit):
+    if commit.get('verification'):
+        if commit.get('verification').get('verified') == True:
+            print("Commit "+  commit.get('sha') +" is valid!")
+            return True
+        else:
+            print("Commit " + commit.get('sha') + " is NOT valid!")
+            return False
+    return False
+
 
 if droneEvent == "push":
-    print("Push event - no verification check required")
-    sys.exit(0)
-
-commits = getData("https://api.github.com/repos/"+ repoOwner + "/"+ repoName + "/pulls/"+ str(pullRequestNumber) +"/commits")
-print("Checking PGP verification status of PR "+ str(pullRequestNumber))
-if len(commits) == 0:
-    print("No commits found")
-    sys.exit(1)
-
-for commit in commits:
-    commitData = getData(commit['url'])
-    if commitData.get('commit').get('verification'):
-        if commitData.get('commit').get('verification').get('verified') == True:
-            print("Commit "+  commitData.get('sha') +" is valid!")
-            isPrValid = True
-        else:
-            print("Commit " + commitData.get('sha') + " is NOT valid!")
-            isPrValid = False
+    commitData = getData("https://api.github.com/repos/"+ repoOwner + "/"+ repoName + "/git/commits/" + os.environ.get('DRONE_COMMIT_SHA'))
+    print("Checking last commit: "+ os.environ.get('DRONE_COMMIT_SHA'))
+    result = checkCommit(commitData)
+    if result:
+        print("All PGP checks passed")
+        sys.exit(0)
     else:
-        isPrValid = False
+        print("PGP checks did not pass")
+        sys.exit(1)
 
-
-if isPrValid:
-    print("All PGP checks passed")
-    sys.exit(0)
 else:
-    print("PGP checks did not pass")
-    sys.exit(1)
+    isPrValid = False
+    commits = getData("https://api.github.com/repos/"+ repoOwner + "/"+ repoName + "/pulls/"+ str(pullRequestNumber) +"/commits")
+    print("Checking PGP verification status of PR "+ str(pullRequestNumber))
+    if len(commits) == 0:
+        print("No commits found")
+        sys.exit(1)
+
+    for commit in commits:
+        commitData = getData(commit['url'])
+        if commitData.get('commit').get('verification'):
+            isPrValid = checkCommit(commitData.get('commit'))
+        else:
+            isPrValid = False
+            break
+
+
+    if isPrValid:
+        print("All PGP checks passed")
+        sys.exit(0)
+    else:
+        print("PGP checks did not pass")
+        sys.exit(1)
